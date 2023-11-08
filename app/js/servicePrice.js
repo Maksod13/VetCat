4ecefb99c0a82c40ef6b6d7541e7d5709f0122eb
Ihor Shevchyk <shevchyk.ihor@gmail.com>
2023-11-08 17:49:03 +0200

    vetcat

--- app/js/servicePrice.js
+++ app/js/servicePrice.js
@@ -6,11 +6,15 @@ let servicePriceBtns = document.querySelectorAll('.servicePrice-btn');
 // all cards
 let servicePriceCards = document.querySelectorAll('.service-price-card');
 
+let page404 = document.querySelector('.servicePrice-section-4');
+
 //показує всі картки
 function showAllCards() {
     servicePriceCards.forEach(card => {
         card.style.display = 'block';
-    })
+    });
+
+    page404.style.display = 'none';
 }
 
 //функція яка показує лише одну картку по кнопкам
@@ -26,13 +30,21 @@ function showOneCard(btn) {
         return;
     }
 
+    page404.style.display = 'none';
+    let cardFound = false;
+
     servicePriceCards.forEach(card => {
         card.style.display = 'none';
         let tempName = card.dataset.cardName;
         if (tempName == dataBtnName) {
             card.style.display = 'block';
+            cardFound = true;
         }
     });
+
+    if(cardFound == false){
+        page404.style.display = 'block';
+    }
 }
 
 //встановлюємо функціонал на кожну кнопку .servicePrice-btn
@@ -47,20 +59,40 @@ let servicePriceSearchInput = document.querySelector('.servicePrice-search-input
 //функція показу карток через інпути
 function showOneCardInput(name) {
 
+    page404.style.display = 'none';
+    let cardFound = false;
     servicePriceCards.forEach(card => {
         card.style.display = 'none';
         let servicePriceCardTitle = card.querySelector('.service-price-card-title');
         let title = servicePriceCardTitle.textContent.trim().toLowerCase();
         if (title == name) {
             card.style.display = 'block';
+            cardFound = true;
         }
     });
+
+    if(cardFound == false){
+        page404.style.display = 'block';
+    }
 }
 
 servicePriceSearchBtn.addEventListener('click', () => {
     let inputValue = servicePriceSearchInput.value.toLowerCase();
     showOneCardInput(inputValue);
+    servicePriceSearchInput.value = '';
 });
 
 
+
+let showAllServiceBtn = document.querySelector('.show-all-service-btn');
+
+showAllServiceBtn.addEventListener('click', showAllItems);
+
+function showAllItems(){
+    servicePriceBtns.forEach(btn => {
+        btn.style.display = "block";
+    });
+
+    showAllServiceBtn.style.display = "none";
+}
 
