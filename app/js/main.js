979bf0b71c3979830d00929394d349d5152c786c
Ihor Shevchyk <shevchyk.ihor@gmail.com>
2023-11-10 22:37:31 +0200

    vetcat

--- app/js/main.js
+++ app/js/main.js
@@ -149,7 +149,33 @@ buttons.forEach((button, index) => {
   });
 });
 
-const swiperStrogino = new Swiper('.swiper-strogino', {
+
+
+const stroginoHospital = document.querySelector(".strogino-hospital");
+const donskoiHospital = document.querySelector(".donskoi-hospital");
+const swiper1 = document.querySelector(".someService-swiper1");
+const swiper2 = document.querySelector(".someService-swiper2");
+
+stroginoHospital.addEventListener("click", () => {
+  swiper1.style.display = "block";
+  swiper2.style.display = "none";
+  donskoiHospital.classList.remove("btn-active");
+  donskoiHospital.classList.add("btn-nonactive");
+  stroginoHospital.classList.remove("btn-nonactive");
+  stroginoHospital.classList.add("btn-active");
+
+});
+
+donskoiHospital.addEventListener("click", () => {
+  swiper1.style.display = "none";
+  swiper2.style.display = "block";
+  stroginoHospital.classList.remove("btn-active");
+  stroginoHospital.classList.add("btn-nonactive");
+  donskoiHospital.classList.remove("btn-nonactive");
+  donskoiHospital.classList.add("btn-active");
+});
+
+const swiperStrogino1 = new Swiper('.swiper-strogino', {
 
   slidesPerView: 1,
   navigation: {
@@ -172,7 +198,7 @@ const swiperStrogino = new Swiper('.swiper-strogino', {
 });
 
 
-const swiperDonskoi = new Swiper('.swiper-donskoi', {
+const swiperDonskoi2 = new Swiper('.swiper-donskoi', {
 
   slidesPerView: 1,
   navigation: {
